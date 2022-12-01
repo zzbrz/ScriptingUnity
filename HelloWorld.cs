e4022e12d1213ba2fba3427cc53106dd2f49429d
JhonRg <rgjonatan@gmail.com>
2022-12-01 17:09:07 +0100

    Starting repository
    My first script in C# for Unity.

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+Velo de Sosiego x15
+Coloso 2 estrellas
+Cho-gat
+
+aumento prismatico y atributos prismaticos
+
+Mercenarios 5/7
+
+Ambas formas transformable
+Jayce
+
+2 componentes iguales para 2 objetos
+Bansky + Bufo azul
+
+
+AUMENTO: Gracia celestial
